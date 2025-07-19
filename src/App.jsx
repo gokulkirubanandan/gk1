b97881526c79ab10d74e4cf892d1609bb0fd3e93
gokulkirubanandan <70764880+gokulkirubanandan@users.noreply.github.com>
2025-07-19 10:35:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/header/Header";
 import Search from "./components/search/Search";
-import AddProducts from "./components/addproducts/AddProducts";
+import AddProducts from "./components/addproducts/Addproducts";
 import CardBody from "./components/cards/CardBody";
 import Button from "./components/button/Button";
 
